1740d71ccd92c67a05e677124668c988d6df2bc2
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-05-27 23:35:42 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,7 +3,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'tonicflexy002gmail.com', // Replace with your Mega email
-    password: 'Tonic@2006', // Replace with your Mega password
+    password: 'Tonic@20066', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
